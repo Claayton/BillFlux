21db1aac3734a92493f4ee8d48483e241e72c712
Clayton Garcia <claaytongarcia@gmail.com>
2024-08-07 13:52:28 -0300

    Feat: Implemented CLI

--- ticketmanager/cli.py
+++ ticketmanager/cli.py
@@ -1,5 +1,32 @@
-from .config import settings
+import typer
+from rich.table import Table
+from rich.console import Console
+from typing import Optional, List
+from ticketmanager.core import add_ticket_to_database, get_tickets_from_database
+from ticketmanager.models import Ticket
+
+main = typer.Typer(help="Ticket Manager Aplication")
+console = Console()
 
 
-def main():
-    print("Hello from", settings.NAME)
+@main.command("add")
+def add(bar_code: int, suplyer: str, type: str = typer.Option(...)):
+    """Add a new ticket to database."""
+    if add_ticket_to_database(bar_code, suplyer, type):
+        print("New ticket add on database!")
+    else:
+        print("Failed! :()")
+
+
+@main.command("list")
+def list_tickets(type: Optional[str] = None) -> List[Ticket]:
+    """Lists tickets in database."""
+    tickets = get_tickets_from_database()
+    table = Table(title="Tickets")
+    headers = ["bar_code", "suplyer", "type", "due_date", "payday", "is_paid_out"]
+    for header in headers:
+        table.add_column(header, style="magenta")
+    for ticket in tickets:
+        values = [str(getattr(ticket, header)) for header in headers]
+        table.add_row(*values)
+    console.print(table)
