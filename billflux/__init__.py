d23c07a6fae00421c84ab3e5b0f175fd07240070
Clayton Garcia <claaytongarcia@gmail.com>
2024-08-21 12:43:32 -0300

    Bugfix: Bug fix after profect name change, test database has been deleted for now

--- billflux/__init__.py
+++ billflux/__init__.py
@@ -1,13 +1,15 @@
 """Module to create the app"""
 
 from flask import Flask
+from dynaconf import FlaskDynaconf
 from billflux.controlers import home, bills
 
 
 def create_app():
+    """Function that creates the app"""
 
     app = Flask(__name__)
+    FlaskDynaconf(app)
     app.register_blueprint(home.bp)
     app.register_blueprint(bills.bp)
-
     return app
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,15 @@
 """Conftest for tests"""
 
 from pytest import fixture
+from billflux import create_app
 from tests.mocks.mock_bills import mock_bill
+
+
+@fixture(scope="module")
+def app():
+    """Configura o aplicativo de teste com um banco de dados em memória."""
+    return create_app()
+
 
 ticket = mock_bill()
 
