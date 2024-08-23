86c051b70ed1100b7132ce703373d49c0bf95001
Clayton Garcia <claaytongarcia@gmail.com>
2024-08-23 13:52:11 -0300

    Refactor: Refatored the file controllers.bills

--- billflux/controlers/bills.py
+++ billflux/controlers/bills.py
@@ -1,4 +1,4 @@
-"""Module to instantiate the blueprint for the bills page"""
+"""File to instantiate the blueprint for the bills page"""
 
 from flask.blueprints import Blueprint
 from flask.templating import render_template
@@ -11,9 +11,9 @@ bp = Blueprint("bp_bills", __name__)
 @bp.route("/bills/", methods=["GET", "POST"])
 @bp.route("/bills", methods=["GET", "POST"])
 def bills():
-    """Mount bills route"""
+    """Mount bills route, and list all bills in the table"""
 
     bills_repository = BillRepository()
-    bills_list = bills_repository.get_bills()
+    list_bills = bills_repository.get_bills()
 
-    return render_template("bills.html", bills_list=bills_list)
+    return render_template("bills.html", bills_list=list_bills)
