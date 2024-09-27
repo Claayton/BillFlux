5cb387d7565913288f229f265247efe449342f4e
Clayton Garcia <claaytongarcia@gmail.com>
2024-09-27 11:37:27 -0300

    Tests: Update tests to bills routes

--- tests/controlers/bills_test.py
+++ tests/controlers/bills_test.py
@@ -1,21 +1,48 @@
 """Tests from bills route"""
 
+from flask_login import login_user
+from billflux.infra.entities import User
 
-def test_bills_1(client):
-    """Testing /bills route"""
+
+def test_bills_1_with_user_logged(client, fake_user):
+    """Testing /bills route, with user logged"""
+
+    url = "/bills"
+    user = User(id=fake_user.id, email=fake_user.email)
+    login_user(user)
+
+    response = client.get(url)
+
+    assert response.status_code == 200
+
+
+def test_bills_1_without_user_logged(client):
+    """Testing /bills route, without user logged"""
 
     url = "/bills"
 
     response = client.get(url)
 
+    assert response.status_code == 302
+
+
+def test_bills_2(client, fake_user):
+    """Testing /bills/ route, with user loged"""
+
+    url = "/bills/"
+    user = User(id=fake_user.id, email=fake_user.email)
+    login_user(user)
+
+    response = client.get(url)
+
     assert response.status_code == 200
 
 
-def test_bills_2(client):
-    """Testing /bills/ route"""
+def test_bills_2_without_user_logged(client):
+    """Testing /bills/ route, without user logged"""
 
     url = "/bills/"
 
     response = client.get(url)
 
-    assert response.status_code == 200
+    assert response.status_code == 302
